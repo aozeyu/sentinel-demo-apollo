ab254a97f4e4f5da92c815f010158c6c82f44ad5
aozeyu <3484766443@qq.com>
2024-05-14 15:12:24 +0800

    misc change

--- src/main/java/com/github/anilople/sentineldemoapollo/controller/EchoController.java
+++ src/main/java/com/github/anilople/sentineldemoapollo/controller/EchoController.java
@@ -1,19 +1,31 @@
 package com.github.anilople.sentineldemoapollo.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 @RestController
 public class EchoController {
   private static final Logger logger = LoggerFactory.getLogger(EchoController.class);
+  private final ApplicationContext applicationContext;
+  public EchoController(ApplicationContext applicationContext) {
+    this.applicationContext = applicationContext;
+  }
 
   @GetMapping("/echo")
+  @SentinelResource(value = "myecho")
   public ResponseEntity<String> echo(@RequestParam String id) {
     logger.info("echo: id = [{}]", id);
     return ResponseEntity.ok(id);
+  }
+
+  @PostMapping("/echo/json/object")
+  public ResponseEntity<Map<String,String>> echoJsonObject(@RequestBody Map<String,String> map) {
+    return ResponseEntity.ok(map);
   }
 }
