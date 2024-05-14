96c79935c529de918f83fff77d92e6285aa6c5e0
aozeyu <3484766443@qq.com>
2024-05-14 14:55:11 +0800

    build: finalName set to project.artifactId

--- src/main/java/com/github/anilople/sentineldemoapollo/controller/EchoController.java
+++ src/main/java/com/github/anilople/sentineldemoapollo/controller/EchoController.java
@@ -1,7 +1,19 @@
 package com.github.anilople.sentineldemoapollo.controller;
 
-import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EchoController {
+  private static final Logger logger = LoggerFactory.getLogger(EchoController.class);
+
+  @GetMapping("/echo")
+  public ResponseEntity<String> echo(@RequestParam String id) {
+    logger.info("echo: id = [{}]", id);
+    return ResponseEntity.ok(id);
+  }
 }
